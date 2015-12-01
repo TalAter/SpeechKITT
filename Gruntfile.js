ec3f636f10ab01b340f1280c3edab42ad6856306
Tal Ater <tal@talater.com>
2015-11-30 23:15:13 -0500

    Added a basic project with jshint, uglify and watch

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: [
+        'src/speech-ui-kitt.js',
+        'Gruntfile.js'
+      ],
+      options: {
+        jshintrc: true
+      }
+    },
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      all: {
+        files: {
+          'dist/speech-ui-kitt.min.js': ['src/speech-ui-kitt.js']
+        }
+      }
+    },
+    watch: {
+      files: ['src/speech-ui-kitt.js', '!**/node_modules/**'],
+      tasks: ['default'],
+    }
+  });
+
+  // Load NPM Tasks
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
