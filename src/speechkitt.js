0f2b6a007c20599b0324504dce85050b46563da5
Tal Ater <tal@talater.com>
2015-12-01 16:23:46 -0500

    Added (non-functional) SpeechKITT.setStart and setAbort methods

--- src/speechkitt.js
+++ src/speechkitt.js
@@ -12,6 +12,59 @@
 
   // Expose functionality
   _root.SpeechKITT = {
+
+    /**
+     * Define the function that should be called in order to start speech recognition.
+     *
+     * #### Examples:
+     * ````javascript
+     * // Will call annyang's start method
+     * SpeechKITT.setStart(annyang.start);
+     * // Will call a global function called listen with a local context.
+     * SpeechKITT.setStart(listen, this);
+     * // Functions can also be stated by name (string)
+     * SpeechKITT.setStart('listen', this);
+     * // Using the browser's native start function
+     * SpeechKITT.setStart(webkitSpeechRecognition.start);
+     * ````
+     *
+     * @param {Function|String} callback - The function to call to start speech recognition
+     * @param {Object} [context] - Optional context for the callback function
+     * @method setStart
+     */
+    setStart: function(callback, context) {
+      // This should work both when passed a function, or a name of a function
+      callback = _root[callback] || callback;
+      if (typeof callback !== 'function') {
+        throw new TypeError('invalid callback function');
+      }
+      context = context || this;
+    },
+
+    /**
+     * Define the function that should be called in order to abort (stop) speech recognition.
+     *
+     * #### Examples:
+     * ````javascript
+     * // Will call annyang's abort method
+     * SpeechKITT.setAbort(annyang.abort);
+     * // Using the browser's native abort function
+     * SpeechKITT.setAbort(webkitSpeechRecognition.abort);
+     * ````
+     *
+     * @param {Function|String} callback - The function to call to abort speech recognition
+     * @param {Object} [context] - Optional context for the callback function
+     * @method setAbort
+     */
+    setAbort: function(callback, context) {
+      // This should work both when passed a function, or a name of a function
+      callback = _root[callback] || callback;
+      if (typeof callback !== 'function') {
+        throw new TypeError('invalid callback function');
+      }
+      context = context || this;
+    }
+
   };
 
 }).call(this);
--- test/spec/BasicSpec.js
+++ test/spec/BasicSpec.js
@@ -7,6 +7,52 @@
       expect(window.SpeechKITT).toEqual(jasmine.any(Object));
     });
 
+    it('should contain setStart method', function () {
+      expect(window.SpeechKITT.setStart).toEqual(jasmine.any(Function));
+    });
+
+    it('should contain setAbort method', function () {
+      expect(window.SpeechKITT.setAbort).toEqual(jasmine.any(Function));
+    });
+
+  });
+
+  describe('SpeechKITT.setStart', function() {
+
+    it('should throw an error when called without a callback function', function () {
+      expect(function() {
+        window.SpeechKITT.setStart();
+      }).toThrowError();
+    });
+
+    it('should throw an error when called with an invalid callback function', function () {
+      expect(function() {
+        window.SpeechKITT.setStart(undefined);
+        window.SpeechKITT.setStart('blerg');
+      }).toThrowError();
+    });
+
+    //@TODO: define test for how it should work
+
+    //@TODO: define test different contexts
+
+  });
+
+  describe('SpeechKITT.setAbort', function() {
+
+    it('should throw an error when called without a callback function', function () {
+      expect(function() {
+        window.SpeechKITT.setAbort();
+      }).toThrowError();
+    });
+
+    it('should throw an error when called with an invalid callback function', function () {
+      expect(function() {
+        window.SpeechKITT.setAbort(undefined);
+        window.SpeechKITT.setAbort('blerg');
+      }).toThrowError();
+    });
+
   });
 
 })();
